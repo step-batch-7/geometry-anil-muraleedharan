902c9695b4c863e22e79c4297867fdb738fdc9e9
Anil E M <anilmuraleedharan@In-Anile.local>
2019-12-12 16:23:07 +0530

    time's up force commit

--- src/smallLine.js
+++ src/smallLine.js
@@ -8,6 +8,20 @@ class Line {
     let point2 = this.point2;
     return `Line { point1: { x: ${point1.x}, y: ${point1.y} }, point2: { x: ${point2.x}, y: ${point2.y} } }`;
   }
+  isEqualTo(otherLine) {
+    let point1ToCheck = otherLine.point1;
+    let point2ToCheck = otherLine.point2;
+
+    let point1 = this.point1;
+    let point2 = this.point2;
+
+    let isInitialPointsEqual =
+      point1ToCheck.x == point1.x && point1ToCheck.y == point1.y;
+    let isFinalPointsEqual =
+      point2ToCheck.x == point2.x && point1ToCheck.y == point2.y;
+
+    return isInitialPointsEqual && isFinalPointsEqual;
+  }
 }
 
 module.exports = { Line };
